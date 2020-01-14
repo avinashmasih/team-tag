8e5339722e96066e09a490111134fb8230d9d8c0
Joseph Newman <paradoxbuilder@gmail.com>
2020-01-14 11:50:03 -0700

    Custom editor for spray can to display the current fill and if it's spraying

---
+++ Assets/Scripts/Editor/SprayCanEditor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(SprayCan))]
+[CanEditMultipleObjects]
+public class SprayCanEditor : Editor
+{
+    SerializedProperty color;
+    SerializedProperty sprayRate;
+    SerializedProperty maxCapacity;
+    SerializedProperty currentFill;
+    SerializedProperty spraying;
+    SerializedProperty refill;
+
+    private void OnEnable()
+    {
+        color = serializedObject.FindProperty("color");
+        sprayRate = serializedObject.FindProperty("SprayRate");
+        maxCapacity = serializedObject.FindProperty("MaxCapacity");
+        currentFill = serializedObject.FindProperty("currentFill");
+        spraying = serializedObject.FindProperty("spraying");
+        refill = serializedObject.FindProperty("RefillRate");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(color);
+        EditorGUILayout.PropertyField(sprayRate);
+        EditorGUILayout.PropertyField(refill);
+        EditorGUILayout.PropertyField(maxCapacity);
+        serializedObject.ApplyModifiedProperties();
+        EditorGUILayout.LabelField($"Current Fill: {currentFill.floatValue} ({spraying.boolValue})");
+    }
+}
