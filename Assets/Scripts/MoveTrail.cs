98aee4ef7bdb5f3bda4e089059392f41a1660b1f
Casey Gatlin <gatlincasey@gmail.com>
2020-01-15 16:02:54 -0700

    Added MoveTrail test script

---
+++ Assets/Scripts/MoveTrail.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTrail : MonoBehaviour
+{
+    public Spray spray;
+    void Update() { transform.position = spray.SprayLocation; }
+}
