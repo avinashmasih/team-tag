8fa16f1d9935c20af404340b2cfe3215444af5a1
Joseph Newman <paradoxbuilder@gmail.com>
2020-01-16 12:07:58 -0700

    Created a script for managing the display of the current paint level.

---
+++ Assets/Scripts/PaintLevel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class PaintLevel : MonoBehaviour
+{
+    public float Min = 21;
+
+    public float Max = 85;
+
+    RectTransform rTransform;
+
+    public void Start()
+    {
+        rTransform = (RectTransform)gameObject.transform;
+    }
+
+    public float FitPercentage(float percentage)
+    {
+        float range = Max - Min;
+        var offset = range * percentage;
+        return offset + Min;
+    }
+
+    public void SetMaskHeight(float percentage)
+    {
+        var oRect = rTransform.sizeDelta;
+        oRect.y = FitPercentage(percentage);
+        rTransform.sizeDelta = oRect;
+    }
+}
