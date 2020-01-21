d1f84cfa73f9e360c8d3ba5a1163c535044ffdf1
Joseph Newman <paradoxbuilder@gmail.com>
2020-01-21 13:38:44 -0700

    Editor for dqitems

---
+++ Assets/Scripts/Editor/DeltaQueueItemEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+[CanEditMultipleObjects]
+[CustomEditor(typeof(DeltaQueue.DeltaQueueItem))]
+class DeltaQueueItemEditor : Editor
+{
+    SerializedProperty time;
+    SerializedProperty item;
+
+    private void OnEnable()
+    {
+        time = serializedObject.FindProperty("time");
+        item = serializedObject.FindProperty("item");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(time);
+        EditorGUILayout.PropertyField(item);
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+
