ea9eed12ed4e36ae326b9267eaa322e2c882a2b1
Casey Gatlin <gatlincasey@gmail.com>
2020-01-21 16:51:30 -0700

    Added fonts, start menu is almost complete

---
+++ Assets/MenuHover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class MenuHover : MonoBehaviour, IPointerEnterHandler, ISelectHandler, IPointerExitHandler
+{
+    public Animator anim;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        anim.SetBool("isHighlighted", true);
+        anim.SetBool("isMirrored", false);
+    }
+    
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        anim.SetBool("isHighlighted", false);
+        anim.SetBool("isMirrored", true);
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+
+    }
+
+
+}
