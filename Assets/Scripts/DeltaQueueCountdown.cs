f8328fff29d702092268c465900bc877b46a95b9
Joseph Newman <paradoxbuilder@gmail.com>
2020-01-22 16:24:13 -0700

    Added a display for the delta queue

---
+++ Assets/Scripts/DeltaQueueCountdown.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DeltaQueueCountdown : MonoBehaviour
+{
+    public Text countdown;
+
+    public DeltaQueue queue;
+
+    private void Start()
+    {
+        queue.OnSecondElapsed += Tick;
+    }
+
+    void Tick(float timeRemaining)
+    {
+        if (!countdown) return;
+        int minutesLeft = (int)timeRemaining / 60;
+        int secondsLeft = (int)timeRemaining % 60;
+        countdown.text = minutesLeft <= 0 ?
+            secondsLeft.ToString() :
+            $"{minutesLeft}:{secondsLeft}";
+    }
+}
---
+++ Assets/Scripts/ObjectiveDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectiveDisplay : MonoBehaviour
+{
+    public Text objectiveText;
+
+    public string finishedText = "Done!";
+
+    public DeltaQueue queue;
+
+    public void Start()
+    {
+        queue.OnClockAdvanced.AddListener(SetText);
+        queue.OnClockCompleted.AddListener(SetTextFinished);
+    }
+
+    public void SetText(string text)
+    {
+        objectiveText.text = text;
+    }
+
+    private void SetTextFinished()
+    {
+        objectiveText.text = finishedText;
+    }
+}
