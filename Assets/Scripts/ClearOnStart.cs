f245234e56c4f996c33761539b72d3ab717950da
Avinash <imavinashmasih@gmail.com>
2020-01-23 03:04:07 -0700

    Added a clear script for wall

---
+++ Assets/Scripts/ClearOnStart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearOnStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = gameObject.GetComponent<MeshFilter>().sharedMesh;
+        Color[] colArray = new Color[mesh.vertices.Length];
+        mesh.colors = colArray;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
