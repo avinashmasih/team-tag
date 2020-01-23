b235369751b9ab72a3bb7a41be87bc42e0b3fb78
Avinash <imavinashmasih@gmail.com>
2020-01-23 01:18:13 -0700

    Added Script to convert to texture with alpha

---
+++ Assets/Scripts/ConvertToAlpha.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConvertToAlpha : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Texture2D Converter(Texture2D initialTex)
+    {
+        int x = initialTex.width;
+        int y = initialTex.height;        
+        Color[] pixels = initialTex.GetPixels(0, 0, x, y);
+        Texture2D finaltex = new Texture2D(x, y);
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                if (initialTex.GetPixel(i, j) == Color.white)
+                {
+                    //Debug.Log("true");
+                    finaltex.SetPixel(i, j, new Color(0, 0, 0, 0));
+                }
+                else
+                {
+                    //Debug.Log("false");
+                    finaltex.SetPixel(i, j, initialTex.GetPixel(i, j));
+                }
+            }
+        }
+        finaltex.Apply();
+        return finaltex;
+    }
+}
