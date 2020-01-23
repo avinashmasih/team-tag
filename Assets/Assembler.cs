657f0c7f2effc6c7e67e0cf1466630bbf6e3c11d
Joseph Newman <paradoxbuilder@gmail.com>
2020-01-22 21:07:14 -0700

    Created an assembler that puts together the objects

---
+++ Assets/Assembler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Puts together the images asked for.
+/// </summary>
+public class Assembler : MonoBehaviour
+{
+    /// <summary>
+    /// The target sprite renderers. These should be in the same order as the
+    /// desired texture locations.
+    /// </summary>
+    public SpriteRenderer[] spriteRenderers;
+
+    /// <summary>
+    /// The texture to sprite script that contains the textures.
+    /// </summary>
+    public TextureToSprite textures;
+
+    /// <summary>
+    /// Puts the images together.
+    /// </summary>
+    public void Assemble()
+    {
+        for (int i = 0; i < spriteRenderers.Length; ++i)
+        {
+            var tex = textures.textureList[i];
+            var sr = spriteRenderers[i];
+
+            sr.sprite = Sprite.Create(
+                tex, 
+                new Rect(0, 0, tex.width, tex.height), 
+                new Vector2(.5f, .5f)
+            );
+        }
+    }
+}
